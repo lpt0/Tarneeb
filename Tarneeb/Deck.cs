467e9ff4febc58984bc07e90fd72193fc4d95090
BadSolitaire <60454905+BadSolitaire@users.noreply.github.com>
2021-01-22 15:52:14 -0500

    Create Deck.cs
    Empty class

---
+++ Tarneeb/Deck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * @author  Andrew Kuo
+ * @date    2021-01-22
+ */
+
+namespace Tarneeb
+{
+    
+    class Deck
+    {
+        
+    }
+}
