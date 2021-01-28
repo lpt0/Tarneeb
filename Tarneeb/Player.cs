d872fbb3aa59027f0e6b5cff42602f93407f5cad
Domenic <domenic.catalano@dcmail.ca>
2021-01-28 14:46:56 -0500

    Player class created

--- Tarneeb/Player.cs
+++ Tarneeb/Player.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * Author: Domenic Catalano
+ * Date: January 22, 2020
+ * Version: 1.0 (Jan 22, 2020)
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +11,9 @@ using System.Threading.Tasks;
 
 namespace Tarneeb
 {
+    /// <summary>
+    /// 
+    /// </summary>
     class Player
     {
 
