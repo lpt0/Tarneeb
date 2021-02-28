744dea81ca39dba5da456f2dce3b98165b17f7de
Haran <haran@nxdomain.ca>
2021-02-28 04:43:43 -0500

    Rework logging
    Instead of one class for all logs, I've added separate classes for each type of log.
    This will be helpful for AI functionality; it can just read each log.

---
+++ TarneebClasses/Logging/BidPlacedLog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/**
+ * Author: Haran
+ * Date: 2021-02-08
+ */
+namespace TarneebClasses.Logging
+{
+    /// <summary>
+    /// A log representing a bid being placed.
+    /// </summary>
+    public class BidPlacedLog : ILog
+    {
+        public Type Type => Type.NEW_GAME;
+
+        public DateTime DateTime => DateTime.Now;
+
+        /// <summary>
+        /// The bid that was played.
+        /// </summary>
+        public Bid Card;
+
+        /// <summary>
+        /// The player that placed the bid.
+        /// </summary>
+        public Player Player;
+
+        public override string ToString()
+        {
+            // TODO
+            return $"[{this.DateTime}] {this.Player.PlayerName} placed a bid of TODO.";
+        }
+    }
+}
---
+++ TarneebClasses/Logging/CardPlayedLog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/**
+ * Author: Haran
+ * Date: 2021-02-08
+ */
+namespace TarneebClasses.Logging
+{
+    /// <summary>
+    /// A log representing a card being played.
+    /// </summary>
+    public class CardPlayedLog : ILog
+    {
+        public Type Type => Type.NEW_GAME;
+
+        public DateTime DateTime => DateTime.Now;
+
+        /// <summary>
+        /// The card that was played.
+        /// </summary>
+        public Card Card;
+
+        /// <summary>
+        /// The player that played the card.
+        /// </summary>
+        public Player Player;
+
+        public override string ToString()
+        {
+            return $"[{this.DateTime}] {this.Player.PlayerName} played {this.Card}.";
+        }
+    }
+}
