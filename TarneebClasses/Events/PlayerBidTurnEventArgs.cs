a7ce7e165cac1f6f94d4f6b0f92dea8cdd12b664
Haran <haran@nxdomain.ca>
2021-03-09 02:41:43 -0500

    Game: Add core game logic

---
+++ TarneebClasses/Events/PlayerBidTurnEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/**
+ * @author: Haran
+ * @date: 2021-02-28
+ */
+namespace TarneebClasses.Events
+{
+    /// <summary>
+    /// Event arguments for whenever it is a player's turn to bid.
+    /// </summary>
+    /// <see cref="EventArgs" />
+    public class PlayerBidTurnEventArgs : EventArgs
+    {
+        /// <summary>
+        /// The player that is currently playing. TODO reword
+        /// </summary>
+        public Player Player { get; set; }
+    }
+}
