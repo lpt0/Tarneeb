7ebb8d89502cc25318fca816aa770fcf9616df36
BadSolitaire <60454905+BadSolitaire@users.noreply.github.com>
2021-03-11 04:35:22 -0500

    Add CardControl.xaml and quick test on MainWindow.xaml. There currently a visual glitch when adding mutliple cards, more info would be needed.

--- Tarneeb/MainWindow.xaml.cs
+++ Tarneeb/MainWindow.xaml.cs
@@ -12,7 +12,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TarneebClasses;
 
+/**
+ * @author  Andrew Kuo, <APPEND_NAME>
+ * 
+ * @date    2021-03-11
+ */
 namespace Tarneeb
 {
     /// <summary>
@@ -20,9 +26,52 @@ namespace Tarneeb
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// MainWindow Constructor.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Window Loaded handler. Runs when Mainwindow is ready.
+        /// </summary>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Call CardControl Test.
+            TestCardControlPlayerHand();
+        }
+
+        /// <summary>
+        /// Generates a hand and put them into the Player's Hand.
+        /// </summary>
+        private void TestCardControlPlayerHand()
+        {
+            // Generate 13 Cards to new Deck.
+            Deck aHand = (new Deck()).Draw(13);
+            
+            // For each Card in Hand, generate CardControls
+            foreach(Card aCard in aHand.Cards)
+            {
+                // Create CardControl with Card. 
+                CardControl cc = new CardControl(aCard);
+                // Attach generic Click event listener.
+                cc.Click += new RoutedEventHandler(Card_Click);
+                // Append the CardControl to the PlayerHand Area.
+                this.PlayerHand.Children.Add(cc);
+            }
+        }
+
+        /// <summary>
+        /// Generic Click event handler for the CardControls.
+        /// </summary>
+        private void Card_Click(object sender, EventArgs e)
+        {
+            // Change sender to CardControl.
+            CardControl cc = sender as CardControl;
+            // Display what Card was 'Click' ed.
+            MessageBox.Show(cc.Card.ToString());
         }
     }
 }
