bbeccb3983807d0e72f2036dddd394288e262a59
Haran <haran@nxdomain.ca>
2021-03-17 02:26:16 -0400

    Logging: Add more log classes

---
+++ TarneebClasses/Logging/BidCompleteLog.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/**
+ * Author: Haran
+ * Date: 2021-03-16
+ */
+namespace TarneebClasses.Logging
+{
+    /// <summary>
+    /// A log representing a bid being placed.
+    /// </summary>
+    public class BidCompleteLog : ILog
+    {
+        public Type Type => Type.BID_PLACED;
+
+        public DateTime DateTime => DateTime.Now;
+
+        /// <summary>
+        /// The winning team.
+        /// </summary>
+        public Enums.Team WinningTeam;
+
+        /// <summary>
+        /// The losing team.
+        /// </summary>
+        public Enums.Team LosingTeam;
+
+        /// <summary>
+        /// The score.
+        /// </summary>
+        public int Score;
+
+        /// <summary>
+        /// Get this log as a string.
+        /// </summary>
+        /// <returns>The string representation of the log.</returns>
+        public override string ToString()
+        {
+            // TODO
+            return $"[{this.DateTime}] {this.WinningTeam} won with a score of {this.Score}.";
+        }
+    }
+}
---
+++ TarneebClasses/Logging/TarneebSuitLog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/**
+ * Author: Haran
+ * Date: 2021-02-16
+ */
+namespace TarneebClasses.Logging
+{
+    /// <summary>
+    /// A log representing a bid being placed.
+    /// </summary>
+    public class TarneebSuitLog : ILog
+    {
+        public Type Type => Type.TARNEEB_SUIT;
+
+        public DateTime DateTime => DateTime.Now;
+
+        /// <summary>
+        /// The tarneeb suit.
+        /// </summary>
+        public Enums.CardSuit Suit;
+
+        /// <summary>
+        /// The player that placed the bid.
+        /// </summary>
+        public Player Player;
+
+        /// <summary>
+        /// Get this log as a string.
+        /// </summary>
+        /// <returns>The string representation of the log.</returns>
+        public override string ToString()
+        {
+            // TODO
+            return $"[{this.DateTime}] {this.Player.PlayerName} decided on the suit {this.Suit}.";
+        }
+    }
+}
