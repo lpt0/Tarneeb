66dd46f2a428813def79c02fb80e31facdbfcc1e
Haran <haran@nxdomain.ca>
2021-03-17 02:45:19 -0400

    TitleScreen: Add link to user manual

--- Tarneeb/TitleScreen.xaml.cs
+++ Tarneeb/TitleScreen.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Diagnostics; // Process
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -27,7 +28,7 @@ namespace Tarneeb
         /// <summary>
         /// Local play clicked, open main game window.
         /// </summary>
-        private void LocalPlayClicked()
+        private void LocalPlayClicked(object sender, EventArgs args)
         {
             //TODO
         }
@@ -35,7 +36,7 @@ namespace Tarneeb
         /// <summary>
         /// Settings button clicked, open settings window.
         /// </summary>
-        private void SettingsClicked()
+        private void SettingsClicked(object sender, EventArgs args)
         {
             //TODO
         }
@@ -43,9 +44,18 @@ namespace Tarneeb
         /// <summary>
         /// Exit button clicked, close program.
         /// </summary>
-        private void ExitClicked()
+        private void ExitClicked(object sender, EventArgs args)
         {
             this.Close(); //TODO
+        }
+
+        /// <summary>
+        /// User manual button clicked, open the user manual.
+        /// </summary>
+        private void ManualClicked(object sender, EventArgs args)
+        {
+            // Let the default handler for HTTP URLs open the page
+            Process.Start("https://gist.github.com/lpt0/4c532ed6474add1e9d32dd9af6098ca6");
         }
     }
 }
