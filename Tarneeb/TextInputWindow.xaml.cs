6dac209c3c1b09b8ab79dd2233dc5cb28ceefc43
Haran <haran@nxdomain.ca>
2021-03-21 07:19:53 -0400

    Add TarneebSuit and TextInput windows

---
+++ Tarneeb/TextInputWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Tarneeb
+{
+    /// <summary>
+    /// Interaction logic for TextInputWindow.xaml
+    /// </summary>
+    public partial class TextInputWindow : Window
+    {
+        public TextInputWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCloseClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
