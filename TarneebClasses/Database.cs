f72ec625235802c1f1306b35ea5be24699030694
Haran <haran@nxdomain.ca>
2021-04-13 20:27:37 -0400

    Database: Specify fields for insertion

--- TarneebClasses/Database.cs
+++ TarneebClasses/Database.cs
@@ -86,7 +86,7 @@ CREATE TABLE Games (GameID INT PRIMARY KEY IDENTITY(1, 1), Start DATETIME);");
         /// <summary>
         /// Statement to add a game outcome into the statistics table.
         /// </summary>
-        private const string STMT_INSERT_OUTCOME = "INSERT INTO Stats VALUES (@DateTime, @GameID, @Outcome);";
+        private const string STMT_INSERT_OUTCOME = "INSERT INTO Stats (DateTime, GameID, Outcome) VALUES (@DateTime, @GameID, @Outcome);";
 
         /// <summary>
         /// TODO
